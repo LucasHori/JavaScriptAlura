0cb7af194c121c73442735500018322b0bf71e70
Your Name <you@example.com>
2024-01-19 23:36:58 -0300

    primeiros passos

---
+++ app.js
@@ -0,0 +1,33 @@
+
+// O que foi desenvolvido na aula
+/*alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = 29;
+let guardaNumero = prompt('Escolha um número entre 1 e 30');
+
+
+if(numeroSecreto == guardaNumero){
+    console.log('Você acertou o número secreto: 29');
+}
+else{
+    console.log('Você errou');
+}
+*/
+
+
+//desafio da aula
+alert('Boas Vindas ao nosso site!');
+let nome = 'Lua';
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+alert('Erro! Preencha todos os campos');
+let mensagemDeErro = 'Erro! Preencha todos os campos'
+alert(mensagemDeErro);
+let guardarNome = prompt('qual seu nome');
+let guardarIdade = prompt('qual sua idade');
+if(guardarIdade >= 18){
+    alert('Pode tirar a habilitação');
+}
+else{
+    alert('não pode tirar a habilitação');
+}
